f6e49b5f57a22b30dacaa0de4cf56bbf56be0391
Lasse Jonassen <lassejonassentech@gmail.com>
2024-11-24 01:32:06 +0100

    Removed bin and obj folders

--- src/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ src/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e46b2dba4dc8403658f4de93d3412e6729c76fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d458f63ce34eeeb2cc8c328ce452cba61637d8d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
